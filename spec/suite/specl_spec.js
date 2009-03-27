15d5895def5f72a033786626099458b599756b90
Corey Innis <corey@coolerator.net>
2009-03-26 20:27:32 -0400

    restructured the file tree in preparation for specs. note: this breaks public/index.html, which is about to go away.

---
+++ spec/suite/specl_spec.js
@@ -0,0 +1,9 @@
+Screw.Unit(function() {
+  describe("Specl", function() {
+    describe(".filter", function() {
+      it("exists", function() {
+        expect(typeof Specl.filter).to(equal, "function");
+      });
+    });
+  });
+});
