4301f00eec8d8a225809b5e95fd5b264f05b2d23
Corey Innis <corey@coolerator.net>
2009-03-27 14:28:57 -0400

    initial spec suite and some clean-up.

--- spec/public/javascripts/application.js
+++ spec/public/javascripts/application.js
@@ -1,17 +1,21 @@
 (function(specl){
   with(specl) {
-    filters['innermost'] = function() {
+    filter('innermost', function() {
       return $(this.element).find('*').size() === 0;
-    };
+    });
     
-    properties['-local-iconic'] = function() {
+    property('-local-iconic', function() {
       $(this.element).css({
         background: "url(" + this.args[0] + "/" + countries[$(this.element).text()].toLowerCase() + ".png) no-repeat"
       });
-    };
-
-    properties['-local-important'] = function() {
+    });
+    
+    property('-local-important', function(){
       $(this.element).css({ border: this.args[0] + ' solid ' + this.args[1] })
-    };
+    });
   }
 })(Specl);
+
+$().ready(function() {
+  Specl.transform('link[type=text/css]');
+});
