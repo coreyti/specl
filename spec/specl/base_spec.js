3f412b0ef2e1299143d178c1619e02149aa3774b
Corey Innis <corey@coolerator.net>
2009-04-10 21:34:29 -0400

    added failing spec for style switching issue

--- spec/specl/base_spec.js
+++ spec/specl/base_spec.js
@@ -13,17 +13,21 @@ Screw.Unit(function(c) { with(c) {
       selector = 'link#test_external';
       head.append('<link id="test_external" rel="stylesheet" type="text/css" href="specl/base_spec.css" />');
     });
-    
+
     after(function() {
       delete($.expr[':']['fresnel']);
       delete(Specl.property_extensions['-local-fraunhofer']);
-      
+
       $('link#test_external').remove();
     });
-    
+
     // it("determine a better way to provide the test styles, without `div#test_css`", pending);
 
     describe(".transform", function() {
+      before(function() {
+        main.html('<div class="inner">some content</div>');
+      });
+
       it("exists", function() {
         expect(typeof Specl.transform).to(equal, "function");
       });
@@ -41,34 +45,57 @@ Screw.Unit(function(c) { with(c) {
         describe("a filter definition", function() {
           it("is created", function() {
             expect(typeof $.expr[':']['fresnel']).to(equal, 'undefined');
-        
+
             Specl.transform(selector);
             expect(typeof $.expr[':']['fresnel']).to(equal, 'function');
           });
-          
+
           // it("works", pending, function() {
           //   expect(body.find('div:inline')).to(be_empty, 'length');
-          // 
+          //
           //   Specl.transform(selector);
           //   expect(body.find('div:inline')).to_not(be_empty, 'length');
           // });
         });
-        
+
         describe("a property definition", function() {
           it("is created", function() {
             expect(typeof Specl.property_extensions['-local-fraunhofer']).to(equal, 'undefined');
-        
+
             Specl.transform(selector);
             expect(typeof Specl.property_extensions['-local-fraunhofer']).to(equal, 'function');
           });
         });
-        
+
         describe("standard css", function() {
+          before(function() {
+            main.addClass('style_01');
+          });
+
+          after(function() {
+            main.removeClass('style_01');
+          });
+
           it("still applies", function() {
-            expect(body.find('div#test_content').css('color')).to_not(equal, $('body').css('color'));
-        
+            var element = main.find('div.inner');
+            expect(element.css('color')).to(match, /(rgb\(0, 128, 0\)|green)/);
+
             Specl.transform(selector);
-            expect(body.find('div#test_content').css('color')).to(equal, 'red');
+            expect(element.css('color')).to(match, /(rgb\(0, 128, 0\)|green)/);
+          });
+        });
+
+        describe("broken style-switching due to setting element[style]", function() {
+          it("fails", function() {
+            var element = main.find('div.inner');
+
+            main.attr('class', 'style_01');
+            Specl.transform(selector);
+            expect(element.css('color')).to(match, /(rgb\(0, 128, 0\)|green)/);
+
+            main.attr('class', 'style_02');
+            Specl.transform(selector);
+            expect(element.css('color')).to(match, /(rgb\(0, 0, 255\)|blue)/);
           });
         });
       });
