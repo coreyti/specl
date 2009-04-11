6b80a310da7c64836a410e05932c3a2201300c69
Corey Innis <corey@coolerator.net>
2009-04-11 18:28:18 -0400

    specl.ie7: moving to Disco built views in the specs

--- spec/extensions/specl.ie7/reference_spec.js
+++ spec/extensions/specl.ie7/reference_spec.js
@@ -1,47 +1,44 @@
 Screw.Unit(function(c) { with(c) {
-  var head, body, main;
-
-  before(function() {
-    head = $('head');
-    body = $('body');
-    main = $('div#test_content');
-
-    var load_ie7 = (typeof IE7 == 'undefined' && $.browser.msie);
-    load_javascript('../../../lib/vendor/ie7.js',        load_ie7);
-    load_javascript('../../../lib/vendor/ie7-recalc.js', load_ie7);
-    
-    if( ! $.browser.msie) {
-      document.recalc = function() {};
-    }
-  });
-  
-  after(function() {
-    // unload_javascripts();
-    // unload_stylesheets();
-    // unload_content();
-
-    body.removeAttr('spec');
-    main.removeAttr('attr');
-  });
-
   describe("specl.ie7 reference (ie7-js)", function() {
+    var head, body, main, list, items;
+
     before(function() {
-      var view = Disco.build(Disco.Namespace("Specl::IE7").View, {});
-      main.html(view);
+      var load_ie7 = (typeof IE7 == 'undefined' && $.browser.msie);
+      load_javascript('../../../lib/vendor/ie7.js',        load_ie7);
+      load_javascript('../../../lib/vendor/ie7-recalc.js', load_ie7);
+
+      if( ! $.browser.msie) {
+        document.recalc = function() {};
+      }
+
+      head = $('head');
+      body = $('body');
+      main = $('div#test_content');
+      main.html(Disco.build(Disco.Namespace("Specl::IE7").View, { items: 50 }));
+
+      list  = main.find('ul#target');
+      items = list.find('li');
+      expect(items.length).to(equal, 50);
     });
     
+    after(function() {
+      // unload_javascripts();
+      // unload_stylesheets();
+      // unload_content();
+
+      body.removeAttr('spec');
+      main.removeAttr('attr');
+    });
+
     describe("element[attr]", function() {
       before(function() {
         body.attr('spec', 'element[attr]');
-        main.attr('attr', '');
+        list.attr('attr', '');
         document.recalc();
       });
     
       it("applies styles", function() {
-        var element = main.find('div.inner');
-    
-        expect(element.html()      ).to(equal, 'some content');
-        expect(element.css('color')).to(match, /(rgb\(0, 128, 0\)|green)/);
+        expect(items.css('color')).to(match, /(rgb\(0, 128, 0\)|green)/);
       });
     });
 
@@ -52,10 +49,7 @@ Screw.Unit(function(c) { with(c) {
       });
     
       it("applies styles", function() {
-        var element = main.find('div.inner');
-    
-        expect(element.html()      ).to(equal, 'some content');
-        expect(element.css('color')).to(match, /(rgb\(0, 0, 255\)|blue)/);
+        expect(items.css('color')).to(match, /(rgb\(0, 0, 255\)|blue)/);
       });
     });
   });
--- spec/extensions/specl.ie7/spec_helper.js
+++ spec/extensions/specl.ie7/spec_helper.js
@@ -2,13 +2,19 @@ Disco.Namespace("Specl::IE7", {
   View: {
     content: function(builder, initial_attributes) {
       with(builder) {
-        div("some content", { 'class': 'inner' });
+        ul({ 'id': 'target' });
       }
     },
 
     methods: {
       after_initialize: function() {
-
+        this.add_items(this.items);
+      },
+      
+      add_items: function(count) {
+        while((count --) > 0) {
+          this.append('<li>item</li>');
+        }
       }
     }
   }
