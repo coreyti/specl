dfec875954187ab66833d65ba98295c701969cbc
Corey Innis <corey@coolerator.net>
2009-04-11 14:01:47 -0400

    specl.ie7: reference spec (ie7-js) is working and using ie7-recalc.js to apply styles.

--- spec/extensions/specl.ie7/reference_spec.js
+++ spec/extensions/specl.ie7/reference_spec.js
@@ -5,33 +5,35 @@ Screw.Unit(function(c) { with(c) {
     head = $('head');
     body = $('body');
     main = $('div#test_content');
+
+    var load_ie7 = (typeof IE7 == 'undefined' && $.browser.msie);
+    load_javascript('../../../lib/vendor/ie7.js',        load_ie7);
+    load_javascript('../../../lib/vendor/ie7-recalc.js', load_ie7);
+    
+    if( ! $.browser.msie) {
+      document.recalc = function() {};
+    }
   });
   
   after(function() {
-    unload_javascripts();
+    // unload_javascripts();
     // unload_stylesheets();
     // unload_content();
 
     body.removeAttr('spec');
     main.removeAttr('attr');
   });
-  
+
   describe("specl.ie7 reference (ie7-js)", function() {
     before(function() {
-      // load_content(function(builder) {
-      //   with(builder) {
-      //     div("some content");
-      //   }
-      // });
-
       main.html('<div class="inner">some content</div>');
-      load_javascript('../../../lib/vendor/ie7.js', $.browser.msie);
     });
     
     describe("element[attr]", function() {
       before(function() {
         body.attr('spec', 'element[attr]');
         main.attr('attr', '');
+        document.recalc();
       });
     
       it("applies styles", function() {
@@ -39,6 +41,20 @@ Screw.Unit(function(c) { with(c) {
     
         expect(element.html()      ).to(equal, 'some content');
         expect(element.css('color')).to(match, /(rgb\(0, 128, 0\)|green)/);
+      });
+    });
+
+    describe(">", function() {
+      before(function() {
+        body.attr('spec', '>');
+        document.recalc();
+      });
+    
+      it("applies styles", function() {
+        var element = main.find('div.inner');
+    
+        expect(element.html()      ).to(equal, 'some content');
+        expect(element.css('color')).to(match, /(rgb\(0, 0, 255\)|blue)/);
       });
     });
   });
